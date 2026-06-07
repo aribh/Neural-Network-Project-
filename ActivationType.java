a1c335339e71c2a301b5e5b71015329ff49d9138
aribh <hassana5@bxscience.edu>
2026-06-06 23:13:40 -0400

    Add files via upload

---
+++ ActivationType.java
@@ -0,0 +1,8 @@
+package com.arib.neuralnetwork;
+
+public enum ActivationType {
+    SIGMOID,
+    RELU,
+    TANH,
+    LINEAR
+}
---
+++ LayerType.java
@@ -0,0 +1,7 @@
+package com.arib.neuralnetwork;
+
+public enum LayerType {
+    INPUT,
+    HIDDEN,
+    OUTPUT
+}
